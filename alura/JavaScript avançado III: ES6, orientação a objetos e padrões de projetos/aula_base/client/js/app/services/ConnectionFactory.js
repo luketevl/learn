853c4a48dceaf80d9620536f82426efac8060516
luketevl <luketevl@gmail.com>
2016-10-13 15:49:02 -0300

    finished course JS advanced III

--- alura/JavaScript avançado III: ES6, orientação a objetos e padrões de projetos/aula_base/client/js/app/services/ConnectionFactory.js
+++ alura/JavaScript avançado III: ES6, orientação a objetos e padrões de projetos/aula_base/client/js/app/services/ConnectionFactory.js
@@ -1,59 +1,82 @@
-const ConnectionFactory = (function(){
+'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-const stores = ['negociacoes'];
-const version = 4;
-const dbName = 'aluraframe';
-var connection = null;
-var close = null;
-return class ConnectionFactory {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  constructor(){
-    throw new Error('nao pode instancia');
-  }
+var ConnectionFactory = function () {
 
-  static getConnection(){
-      return new Promise((resolve, reject) => {
-          const openRequest = window.indexedDB.open(dbName, version);
-          openRequest.onupgradeneeded = function(e){
-            console.log("Cria ou altera um banco já existente");
-            ConnectionFactory._createStore(e.target.result);
+    var stores = ['negociacoes'];
+    var version = 4;
+    var dbName = 'aluraframe';
 
-          };
-          openRequest.onsuccess = function(e){
-            console.log('Conexão feita');
-            resolve(e.target.result);
-            if(!connection){
-              connection = e.target.result;
-              close = connection.close.bind(connection);
-              connection.close = () => { throw new Error('nao pode fechar a connection') };
+    var connection = null;
+
+    var close = null;
+
+    return function () {
+        function ConnectionFactory() {
+            _classCallCheck(this, ConnectionFactory);
+
+            throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        }
+
+        _createClass(ConnectionFactory, null, [{
+            key: 'getConnection',
+            value: function getConnection() {
+
+                return new Promise(function (resolve, reject) {
+
+                    var openRequest = window.indexedDB.open(dbName, version);
+
+                    openRequest.onupgradeneeded = function (e) {
+
+                        ConnectionFactory._createStores(e.target.result);
+                    };
+
+                    openRequest.onsuccess = function (e) {
+
+                        if (!connection) {
+                            connection = e.target.result;
+                            close = connection.close.bind(connection);
+                            connection.close = function () {
+                                throw new Error('Você não pode fechar diretamente a conexão');
+                            };
+                        }
+                        resolve(connection);
+                    };
+
+                    openRequest.onerror = function (e) {
+
+                        console.log(e.target.error);
+
+                        reject(e.target.error.name);
+                    };
+                });
             }
-            resolve(connection)
-          };
-          openRequest.onerror = function(e){
-            console.log('Erro na conexão: ', e.target.error);
-            reject(e.target.error);
-          };
+        }, {
+            key: '_createStores',
+            value: function _createStores(connection) {
 
-      });
-  }
+                stores.forEach(function (store) {
 
-  static _createStore(connection){
-    stores.forEach(item => {
-      if(connection.objectStoreNames.contains(item)){
-        connection.deleteObjectStore(item);
-      }
-      else{
-        connection.createObjectStore(item, {autoIncrement: true});
-      }
-    });
-  }
+                    if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+                    connection.createObjectStore(store, { autoIncrement: true });
+                });
+            }
+        }, {
+            key: 'closeConnection',
+            value: function closeConnection() {
 
-  static closeConnection(){
-    if(connection){
-      close();
-      connection = null;
-    }
-  }
-}
-})();
+                if (connection) {
+                    close();
+                    connection = null;
+                    close = null;
+                }
+            }
+        }]);
+
+        return ConnectionFactory;
+    }();
+}();
+//# sourceMappingURL=ConnectionFactory.js.map
--- alura/JavaScript avançado III: ES6, orientação a objetos e padrões de projetos/aula_base/client/js/app/dao/NegociacaoDAO.js
+++ alura/JavaScript avançado III: ES6, orientação a objetos e padrões de projetos/aula_base/client/js/app/dao/NegociacaoDAO.js
@@ -1,63 +1,94 @@
-class NegociacaoDAO{
-  constructor(connection){
-    this._connection = connection;
-    this._store = 'negociacoes';
-  }
+'use strict';
 
-  add(negociacao) {
-    return new Promise((resolve, reject) => {
-      const request = this._connection.transaction([this._store], 'readwrite')
-        .objectStore(this._store)
-        .add(negociacao);
-          request.onsuccess = e => {
-            console.log('negociacao add');
-            resolve();
-          };
-          request.onerror = e => {
-            console.log('negociacao fake ', e.target.error);
-            reject(e.target.error);
-          };
-    });
-  }
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  list() {
-    return new Promise((resolve, reject) => {
-      const cursor = this._connection.transaction([this._store], 'readwrite')
-        .objectStore(this._store)
-        .openCursor();
-        let negociacoes = [];
-        cursor.onsuccess = function(e){
-          if(e.target.result){
-            const dado = e.target.result.value;
-            negociacoes.push(new Negociacao(dado._data, dado._quantidade, dado._valor));
-            e.target.result.continue();
-          }
-          else{
-            console.log('negociacao add', negociacoes);
-            resolve(negociacoes);
-          }
-        };
-        cursor.onerror = function(e){
-          console.log('negociacao error ', e.target.error);
-          reject(e.target.error);
-        };
-    });
-  }
+var NegociacaoDao = function () {
+    function NegociacaoDao(connection) {
+        _classCallCheck(this, NegociacaoDao);
 
-  delete() {
-    return new Promise((resolve, reject) => {
-      const request = this._connection.transaction([this._store], 'readwrite')
-        .objectStore(this._store)
-        .clear();
-        request.onsuccess = e => {
-          console.log('negociacao deleted');
-          resolve(true);
-        };
-        request.onerror = e => {
-          console.log('negociacao deleted fail ', e.target.error);
-          reject(e.target.error);
-        };
-    });
-  }
-}
+        this._connection = connection;
+        this._store = 'negociacoes';
+    }
+
+    _createClass(NegociacaoDao, [{
+        key: 'adiciona',
+        value: function adiciona(negociacao) {
+            var _this = this;
+
+            return new Promise(function (resolve, reject) {
+
+                var request = _this._connection.transaction([_this._store], 'readwrite').objectStore(_this._store).add(negociacao);
+
+                request.onsuccess = function (e) {
+
+                    resolve();
+                };
+
+                request.onerror = function (e) {
+
+                    console.log(e.target.error);
+                    reject('Não foi possível adicionar a negociação');
+                };
+            });
+        }
+    }, {
+        key: 'listaTodos',
+        value: function listaTodos() {
+            var _this2 = this;
+
+            return new Promise(function (resolve, reject) {
+
+                var cursor = _this2._connection.transaction([_this2._store], 'readwrite').objectStore(_this2._store).openCursor();
+
+                var negociacoes = [];
+
+                cursor.onsuccess = function (e) {
+
+                    var atual = e.target.result;
+
+                    if (atual) {
+
+                        var dado = atual.value;
+
+                        negociacoes.push(new Negociacao(dado._data, dado._quantidade, dado._valor));
+
+                        atual.continue();
+                    } else {
+
+                        resolve(negociacoes);
+                    }
+                };
+
+                cursor.onerror = function (e) {
+
+                    console.log(e.target.error);
+                    reject('Não foi possível listar as negociações');
+                };
+            });
+        }
+    }, {
+        key: 'apagaTodos',
+        value: function apagaTodos() {
+            var _this3 = this;
+
+            return new Promise(function (resolve, reject) {
+
+                var request = _this3._connection.transaction([_this3._store], 'readwrite').objectStore(_this3._store).clear();
+
+                request.onsuccess = function (e) {
+                    return resolve('Negociações apagadas com sucesso');
+                };
+
+                request.onerror = function (e) {
+                    console.log(e.target.error);
+                    reject('Não foi possível apagar as negociações');
+                };
+            });
+        }
+    }]);
+
+    return NegociacaoDao;
+}();
+//# sourceMappingURL=NegociacaoDao.js.map
